07e1100e162f3607c3730522dd1dea691b0293eb
adamws <adamws@users.noreply.github.com>
2023-07-24 20:59:23 +0200

    Major UI rework, start using josdejong/jsoneditor

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
-import ElementPlus from "element-plus";
-import "element-plus/theme-chalk/index.css";
+import "./style.css";
 import App from "./App.vue";
 
 const app = createApp(App);
-app.use(ElementPlus);
 app.mount("#app");
